7d7235db1f62c4e4900c36f77d7ff30f5d17423f
Eduardo David <davidgripho@gmail.com>
2019-10-15 19:51:54 -0600

    commit push

--- EX10801199209332/routes/api/fotografias/index.js
+++ EX10801199209332/routes/api/fotografias/index.js
@@ -8,6 +8,41 @@ var galeria=fileModel.getFoto();
 router.get('/all', function(req, res){
   galeria = fileModel.getFoto();
   res.json(galeria);
-});
+});// fin del get
+
+
+
+router.post('/new', function(req, res){
+   galeria = fileModel.getFoto();
+   var newFoto = Object.assign(
+      {},
+      req.body,
+      {
+          "titulo": req.body.titulo,
+          "url": req.body.url,
+          "thumb":req.body.thumb
+      }
+   );
+   var fotoExists = galeria.find(
+     function(o, i){
+       return o.id === newFoto.id;
+     }
+   )
+   if( ! fotoExists ){
+     galeria.push(newFoto);
+     fileModel.setFoto(
+        galeria,
+        function(err, savedSuccesfully){
+          if(err){
+            res.status(400).json({ "error": "No se pudo ingresar objeto" });
+          } else {
+            res.json(newFoto);  // req.body ===  $_POST[]
+          }
+        }
+      );
+   } else {
+     res.status(400).json({"error":"No se pudo ingresar objeto"});
+   }
+});// fin del post
 
 module.exports=router;
